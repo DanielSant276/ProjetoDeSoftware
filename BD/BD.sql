c2cacdf577356dac5d6df0bc3efd6908ad805081
Daniel Sant' Anna Andrade <62079449+DanielSant276@users.noreply.github.com>
2022-06-20 21:24:41 -0300

    Criado metade do banco de dados físico

---
+++ BD/BD.sql
@@ -0,0 +1,41 @@
+CREATE DATABASE livrariabd;
+
+CREATE TABLE cliente (
+  idCliente INT AUTO_INCREMENT,
+  clienteQtdLocada INT NOT NULL,
+  clienteTelefone VARCHAR(255) NOT NULL,
+  clienteEndereco VARCHAR(255) NOT NULL,
+  clienteCPF VARCHAR(255) NOT NULL UNIQUE,
+  clienteNome VARCHAR(255) NOT NULL,
+  PRIMARY KEY (idCliente)
+);
+
+CREATE TABLE dependente (
+  idDependente INT AUTO_INCREMENT,
+  depResponsavel VARCHAR(255) NOT NULL,
+  PRIMARY KEY (idDependente) 
+);
+
+CREATE TABLE clienteDependente (
+  idCliente INT NOT NULL,
+  idDependente INT NOT NULL,
+  FOREIGN KEY (idCliente) REFERENCES cliente (idCliente),
+  FOREIGN KEY (idDependente) REFERENCES dependente (idDependente)
+);
+
+CREATE TABLE locacao (
+  idLocacao INT AUTO_INCREMENT,
+  locacaoMulta FLOAT,
+  locacaoPreco FLOAT NOT NULL,
+  dataDevolucao DATE NOT NULL,
+  dataLocada DATE NOT NULL,
+  locacaoEstado VARCHAR(255),
+  PRIMARY KEY (idLocacao)
+);
+
+CREATE TABLE clienteLocacao (
+  idCliente INT NOT NULL,
+  idLocacao INT NOT NULL,
+  FOREIGN KEY (idCliente) REFERENCES cliente (idCliente),
+  FOREIGN KEY (idLocacao) REFERENCES locacao (idLocacao) 
+)
