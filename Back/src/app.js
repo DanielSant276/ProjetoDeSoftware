5b7f392e87c3f86b759d06ada988f239a5e0b726
Daniel Sant' Anna Andrade <62079449+DanielSant276@users.noreply.github.com>
2022-06-27 15:53:55 -0300

    Realizado teste de utilização de cifra de substituição e de Vigenère

--- Back/src/app.js
+++ Back/src/app.js
@@ -1,8 +1,78 @@
 const express = require("express");
 const app = express();
+const bp = require("body-parser");
+
+app.use(bp.urlencoded({ extended: true })); 
 
 app.get("/", function (req, res){
+  console.log("metodo get");
   res.sendFile(__dirname + "/teste/index.html");
+});
+
+app.post("/", function (req, res){
+  console.log(req.body);
+
+  let login = req.body.login;
+  let password = req.body.pass;
+  let key = req.body.key;
+  
+  let newPass = changePass(password.toUpperCase(), key);
+
+  //Chama a criptografia de valores
+  function changePass (pass, key) {
+    const alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","0","1","2","3","4","5","6","7","8","9"];
+
+    console.log(isNaN(parseInt(key)));
+    //Verifica se a chave é numérica ou textual
+    if (isNaN(parseInt(key))) {
+      //Chave textual utiliza a cifra de Vigenère
+      cryptoPass = changePassVigenere(pass, key, alphabet);
+      type = 'vigenère';
+    }
+    else {
+      // chave numérica utiliza cifra simples de substituição
+      key = parseInt(key);
+      cryptoPass = changePassNumberKey(pass, key, alphabet);
+      type = 'substituição'
+    }
+    
+    return cryptoPass.join("");
+  }
+
+  function changePassVigenere(pass, key, alphabet) {
+    let indexPass, indexKey;
+    let cryptoPass = [];
+
+    for (i = 0; i < pass.length; i++) {
+      indexPass = alphabet.indexOf(pass[i]);
+      indexKey = alphabet.indexOf(key[i % key.length]);
+
+      if (indexPass + indexKey > alphabet.length) {
+        cryptoPass[i] = alphabet[indexPass + indexKey - alphabet.length]; 
+      }
+      else {
+        cryptoPass[i] = alphabet[indexPass + indexKey]; 
+      }
+    }
+    return cryptoPass;
+  }
+
+  function changePassNumberKey(pass, key, alphabet) {
+    let index;
+    let cryptoPass = [];
+
+    for (i = 0; i < pass.length; i++) {
+      index = alphabet.indexOf(pass[i]);
+      if (index + key > alphabet.length) {
+        cryptoPass[i] = alphabet[index + key - alphabet.length]; 
+      }
+      else {
+        cryptoPass[i] = alphabet[index + key]; 
+      }
+    }
+    return cryptoPass;
+  }
+  res.send(`Olá ${login}, sua senha é ${password}, sua senha criptografada é ${newPass}, você utilizou a cifra de ${type}`);
 });
 
 app.listen(3000, function() {
