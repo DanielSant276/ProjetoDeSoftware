cb9e1224c9069889bf8714ce844f38c34f716266
Luan Gadioli <Luangmberto2504@gmail.com>
2022-07-06 18:05:43 -0300

    Tela de senha de segurança adicionada

--- Front/src/components/senhaSegura.js
+++ Front/src/components/senhaSegura.js
@@ -1,10 +1,34 @@
 import './App.css';
+import linhasimg from "./img/linhas.svg"
+import olhoimg from "./img/olho.svg"
 
 function App() {
 
   return (
 
-   
+    <div className='principal'>
+      <div className='container'>
+        <div className='box1'>
+          <div className='imgSenhaSegura'>
+            <img className='linhasImg' src={linhasimg} alt=""/>
+            <img className='olhoImg' src={olhoimg} alt=""/>
+          </div>
+          <div className='fraseAtencao' >
+            <h1>ATENÇÃO!</h1>
+          </div>
+        </div>
+        <div className='box2'>
+          <h1 className='titulo space-40'>UTILIZE SENHAS SEGURAS</h1>
+        </div>
+        <div className='box3'>
+            <p>* Sua senha deve possuir de 8 à 16 caracteres</p>
+            <p>*Sua senha deve incluir caractéres especiais </p>
+            <p>( “@”,” #”,”_”,”!” )</p>
+            <p>*Sua senha deve incluir letras MAIUSCÚLAS</p>
+            <p>*Sua senha deve incluir NÚMEROS (0 à 10)</p>
+        </div>
+      </div>
+    </div>
      
   );
 }
