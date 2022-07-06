f7ef3599175552dccf8fc03af680d22f417cc570
anaclaraac <72029578+anaclaraac@users.noreply.github.com>
2022-07-06 11:50:45 -0300

    Update App.js

--- Front/src/App.js
+++ Front/src/App.js
@@ -1,8 +1,47 @@
+import React from 'react'
+import { useForm } from "react-hook-form";
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from "yup";
 import './App.css';
 
+const schema = yup.object({
+  USUARIO: yup.string().required(),
+  SENHA: yup.string().min(8).required(),
+}).required();
+
 function App() {
+  
+  const { register, handleSubmit, formState: { errors } } = useForm({
+    resolver: yupResolver(schema)
+  }); 
+
+  function onSubmit(userData){
+    console.log(userData)
+  } 
   return (
-      <div class="formulario">
+    
+    <div class='loginform'>
+    <form class="conteudoForm" onSubmit={handleSubmit(onSubmit)}>
+      <h1>Bem vindo,</h1>
+      <main class="container">
+      <label>
+        USUÁRIO
+        <input { ... register("USUARIO", { required: true })}/>
+        {errors.USUARIO && <span>PREENCHA O CAMPO</span>}
+      </label>
+
+      <label>
+        SENHA
+        <input { ... register("SENHA", { required: true })}/>
+        {errors.SENHA && <span>PREENCHA O CAMPO</span>}
+      </label>
+
+      <button type='submit'>ENTRAR</button>
+      </main>
+    </form>
+    </div>
+    
+      /*<div class="formulario">
         <form name="form" class="conteudoForm" >
             <div>
                 <label>USUÁRIO</label>
@@ -15,7 +54,7 @@ function App() {
             </div>
         </form>   
         <button class="submit" onclick="todasFunc()">Enviar</button>   
-    </div>
+    </div>*/
   );
 }
 
