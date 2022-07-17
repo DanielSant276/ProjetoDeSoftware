452d68a3cfbc01eb7b2bea74d10f6f86e9b58522
Luan Gadioli <Luangmberto2504@gmail.com>
2022-07-17 18:38:27 -0300

    tela menu

--- Front/src/components/menu/menu.js
+++ Front/src/components/menu/menu.js
@@ -1,26 +1,49 @@
 import React from 'react'
 import '../menu/menu.css'
+import imagem from '../../img/Polygon.svg'
 
 function Menu() {
   return (
-    <header className="menu-header menu-background-imagem alinha-centro">
-      <section className="menu-section">
-        <div className="menu-botoes">
-          
-          <ul className="menu-lista">
-            <li className="menu-botao">
-              <span className="menu-span">Menu 1</span>
-            </li>
-            <li className="menu-botao">
-              <span className="menu-span">Menu 1</span>
-            </li>
-            <li className="menu-botao">
-              <span className="menu-span">Menu 1</span>
-            </li>
-          </ul>
+    <div className='principal menu-background-imagem'>
+      <div className='menu-cabeçalho linha'>
+        <div className='menu-cabeçalho-esquerdo'>
         </div>
-      </section>
-    </header>
+        <div className='menu-botoes linha alinha-centro'>
+          <div className='menu-botao-painel margem-direita  alinha-centro'>
+            <p>PAINEL</p>
+          </div>
+          <div className='menu-botao-painel margem-direita  alinha-centro'>
+            <p>CONFIGURAÇÃO</p>
+          </div>
+          <div className='menu-botao-painel  alinha-centro'>
+            <p>CADASTRO</p>
+          </div>
+        </div>
+        <div className='menu-cabeçalho-direito'>
+        </div>
+      </div>
+      <div className='menu-bem-vindo '>
+        <h1>Bem Vindo,</h1>
+        <h1>__GERENTE__.</h1>
+      </div>
+      <div className='menu-mais-acessados linha'>
+        <img src={imagem} alt=""></img>
+        <h1>MAIS ACESSADOS</h1>
+      </div>
+      <div className='menu-botoes-principais espaco-entre'>
+        <div className='menu-botao-principal-1 alinha-centro menu-botao-principal menu-box-sahedow'>
+          <p>BUSCAR LIVRO</p>
+        </div>
+        <div className='menu-botao-principal-2 alinha-centro menu-botao-principal menu-box-sahedow'>
+          <p>CADASTRAR FUNCIONÁRIO</p>
+        </div>
+        <div className='menu-botao-principal-3 alinha-centro menu-botao-principal menu-box-sahedow'>
+          <p>QUADRO DE MULTAS</p>
+        </div>
+
+      </div>
+
+    </div>
   )
 }
 
