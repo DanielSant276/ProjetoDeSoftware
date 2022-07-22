a4ef6fa60cccfe0810db75093532292199ca30e2
anaclaraac <72029578+anaclaraac@users.noreply.github.com>
2022-07-22 18:55:57 -0300

    Update multasInfo.js

--- Front/src/components/multasInfo/multasInfo.js
+++ Front/src/components/multasInfo/multasInfo.js
@@ -4,99 +4,97 @@ import polygon from "../../img/Polygon.svg";
 
 function MultasInfo() {
   return (
-    <div className="cadastrar-produto-background-imagem principal">
-      <div className="cadastrar-produto-container1" >
-        <div className="cadastrar-produto-conteudo-cabeçalho">
-          <div className="cadastrar-produto-cabeçalho-esquerdo">
+    <div className="multas-info-background-imagem principal">
+      <div className="multas-info-container1" >
+        <div className="multas-info-conteudo-cabeçalho">
+          <div className="multas-info-cabeçalho-esquerdo">
             <h1 className="negrito">PAINEL gerente</h1>
           </div>
-
-          <div className="cadastrar-produto-cabeçalho-direito linha link">
-            <h1 className="negrito">RETORNAR</h1>
-            <img className="cadastrar-produto-seta" src={polygon} alt="" />
-          </div>
-        </div>
-
-        <div className="cadastrar-produto-container2">
-          <div className="cadastrar-produto-cadastrar-produto">
-            <div className="cadastrar-produto-botoes-form linha">
-              <div className="cadastrar-produto-menu-superior-botao botao-form-cadastrar alinha-centro">
-                <p className="cadastrar-produto-menu-superior-botao-texto negrito">LISTAR TUDO</p>
-              </div>
-              <div className="cadastrar-produto-menu-superior-botao botao-form-cadastrar alinha-centro">
-                <p className="cadastrar-produto-menu-superior-botao-texto negrito">BUSCAR CLIENTE</p>
-              </div>
-              <div className="cadastrar-produto-menu-superior-botao botao-form-cadastrar alinha-centro">
-                <p className="cadastrar-produto-menu-superior-botao-texto negrito">CADASTRAR CLIENTE</p>
-              </div>
-              <div className="cadastrar-produto-menu-superior-botao botao-form-cadastrar alinha-centro">
-                <p className="cadastrar-produto-menu-superior-botao-texto negrito">XXXXXXXX</p>
-              </div>
+    
+            <div className="multas-info-cabeçalho-direito linha link">
+              <h1 className="negrito">RETORNAR</h1>
+              <img className="multas-info-seta" src={polygon} alt="" />
             </div>
-
-            <div className="cadastrar-produto-form-cadastro">
-              <div className="cadastrar-produto-form-titulo">
-                <h1 className="negrito laranja">QUADRO DE MULTAS: informações</h1>
-              </div>
-
-              <div className="cadastrar-produto-formulario">
-                <div className="cadastrar-produto-form-apenas linha">
-                  <div className="cadastrar-produto-form-titulo2">
+        </div>
+    
+            <div className="multas-info-container2">
+              <div className="multas-info-multas-info">
+                  <div className="multas-info-botoes-form linha">
+                    <div className="multas-info-menu-superior-botao botao-form-cadastrar alinha-centro">
+                      <p className="multas-info-menu-superior-botao-texto negrito">LISTAR TUDO</p>
+                    </div>
+                    <div className="multas-info-menu-superior-botao botao-form-cadastrar alinha-centro">
+                      <p className="multas-info-menu-superior-botao-texto negrito">BUSCAR CLIENTE</p>
+                    </div>
+                    <div className="multas-info-menu-superior-botao botao-form-cadastrar alinha-centro">
+                      <p className="multas-info-menu-superior-botao-texto negrito">CADASTRAR CLIENTE</p>
+                    </div>
+                    <div className="multas-info-menu-superior-botao botao-form-cadastrar alinha-centro">
+                      <p className="multas-info-menu-superior-botao-texto negrito">XXXXXXXXXX</p>
+                    </div>
+                  </div>
+    
+                  <div className="multas-info-form-cadastro">
+                  <div className="multas-info-form-titulo">
+                    <h1 className="negrito laranja">QUADRO DE MULTAS: informações</h1>
+                  </div>
+    
+                  
+                  <div className="multas-info-formulario">
                     <h1 className="negrito">LOCAÇÃO #101</h1>
+                      <div className="multas-info-form-apenas">        
+                          <div className="multas-info-input linha">
+                            <label className="label-form-cadastrar" >ALOCADO POR:</label>
+                            <input className="input-form-cadastrar" type="text" />
+
+                            <label className="label-form-cadastrar">ID PRODUTO:</label>
+                            <input className="input-form-cadastrar" type="text" />
+
+                            <label className="label-form-cadastrar">VALOR DA MULTA:</label>
+                            <input className="input-form-cadastrar" type="text" />
+
+                            <label className="label-form-cadastrar">ALUGADO POR:</label>
+                            <input className="input-form-cadastrar" type="text" />
+
+                            <label className="label-form-cadastrar">DATA DE LOCAÇÃO:</label>
+                            <input className="input-form-cadastrar" type="text" />
+                            <p className="dependente cinza negrito link">dependente *?*</p>
+                          </div>    
+                      </div>
                   </div>
-                  <div className="cadastrar-produto-input-linha1 linha">
-                    
-                    <label className="label-form-cadastrar" >ALOCADO POR:</label>
-                    <input className="input-form-cadastrar" type="text" />
-
-                    <label className="label-form-cadastrar">ID PRODUTO:</label>
-                    <input className="input-form-cadastrar" type="text" />     
-
-                    <label className="label-form-cadastrar">ALUGADO POR:</label>
-                    <input className="input-form-cadastrar" type="text" />
+                </div>
+              </div>
+    
+              <div className="multas-info-outros-painel">
+                <div className="multas-info-outros-titulos">
+                  <h1>OUTROS</h1>
+                </div>
+                <div className="multas-info-outros-botoes">
+                  <div className="multas-info-outros-botao botao-form-cadastrar link">
+                    <p>CLIENTES</p>
                   </div>
-
-                  <div className="cadastrar-produto-input-linha2 linha">
-                    <label className="label-form-cadastrar">DATA DA LOCAÇÃO:</label>
-                    <input className="input-form-cadastrar" type="text" />
-
-                    <label className="label-form-cadastrar">VALOR DA MULTA:</label>
-                    <input className="input-form-cadastrar" type="text" ></input>
+                  <div className="link">
+                    <p>CADASTRAR</p>
+                  </div>
+                  <div className="link">
+                    <p>BUSCAR CLIENTES</p>
+                  </div>
+                  <div className="link">
+                    <p>LOCAÇÃO</p>
+                  </div>
+                  <div className="link">
+                    <p>DEVOLUÇÃO</p>
+                  </div>
+                  <div className="space-40 link">
+                    <p>MULTAS</p>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-
-          <div className="cadastrar-produto-outros-painel">
-            <div className="cadastrar-produto-outros-titulo">
-              <h1>OUTROS</h1>
-            </div>
-            <div className="cadastrar-produto-outros-botoes">
-              <div className="cadastrar-produto-outros-botao botao-form-cadastrar link">
-                <p>CLIENTES</p>
-              </div>
-              <div className="link">
-                <p>CADASTRAR</p>
-              </div>
-              <div className="link">
-                <p>BUSCAR CLIENTES</p>
-              </div>
-              <div className="link">
-                <p>LOCAÇÃO</p>
-              </div>
-              <div className="link">
-                <p>DEVOLUÇÃO</p>
-              </div>
-              <div className="space-40 link">
-                <p>MULTAS</p>
-              </div>
-            </div>
-          </div>
         </div>
-      </div>
-    </div>
-  );
-}  
-
+      );
+    }
+    
+    
 export default MultasInfo;
