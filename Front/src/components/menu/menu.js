f8bc8d406771ffdfe68fef02b540f2d12f524f53
Letícia Oliveira <71741286+lrylet@users.noreply.github.com>
2022-08-04 00:06:35 -0300

    Adicionado estrutura dos modais
    ainda precisa arrumar o css pra ficar alinhado e do tamanho certo

--- Front/src/components/menu/menu.js
+++ Front/src/components/menu/menu.js
@@ -1,8 +1,38 @@
-import React from 'react'
-import '../menu/menu.css'
-import imagem from '../../img/Polygon.svg'
+import React, { useState } from 'react';
+import '../menu/menu.css';
+import imagem from '../../img/Polygon.svg';
+import Modal from 'react-modal';
+import Multas from '../multas/multas';
 
 function Menu() {
+
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  
+  const styleModal = {
+    content: {
+      width: '80%',
+      margin: '0 auto',
+      background: 'transparent',
+      border: '0',
+    },
+    overlay: {
+      background: 'rgba(0, 0, 0, 0.9)',
+      padding: 0,
+    },
+  };
+
+  const openModal = () => {
+    setModalIsOpen(true);
+  }
+
+  const afterOpenModal = () => {
+
+  }
+  
+  const closeModal = () => {
+    setModalIsOpen(false);
+  }
+
   return (
     <div className='principal menu-background-imagem'>
       <div className='menu-cabeçalho linha'>
@@ -31,17 +61,31 @@ function Menu() {
         <h1>MAIS ACESSADOS</h1>
       </div>
       <div className='menu-botoes-principais espaco-entre'>
-        <div className='menu-botao-principal-1 alinha-centro menu-botao-principal menu-box-sahedow negrito'>
+        {/* implementar um switch entre os modais, por enquanto, abre o mesmo;
+            achar o erro de css que faz a página ficar desconfigurada e sem os tamanhos certos */}
+        <div className='menu-botao-principal-1 alinha-centro menu-botao-principal menu-box-sahedow negrito' onClick={openModal}>
           <p>BUSCAR LIVRO</p>
         </div>
-        <div className='menu-botao-principal-2 alinha-centro menu-botao-principal menu-box-sahedow negrito'>
+        <div className='menu-botao-principal-2 alinha-centro menu-botao-principal menu-box-sahedow negrito' onClick={openModal}>
           <p>CADASTRAR FUNCIONÁRIO</p>
         </div>
-        <div className='menu-botao-principal-3 alinha-centro menu-botao-principal menu-box-sahedow negrito'>
+        <div className='menu-botao-principal-3 alinha-centro menu-botao-principal menu-box-sahedow negrito' onClick={openModal}>
           <p>QUADRO DE MULTAS</p>
         </div>
 
       </div>
+      <Modal
+      isOpen={modalIsOpen}
+      onAfterOpen={afterOpenModal}
+      onRequestClose={closeModal}
+      style={styleModal}
+      handleClick = {closeModal}
+    >
+    <Multas
+      isOpen = {modalIsOpen}
+      handleClick = {closeModal}
+      />
+    </Modal>
 
     </div>
   )
