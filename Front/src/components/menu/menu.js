06343ecc88d5e885a9aa6e359af0869a8ff956a0
Letícia Oliveira <71741286+lrylet@users.noreply.github.com>
2022-08-16 23:42:18 -0300

    Ajustes no menu
    Alterei, além do menu.js e menu.css a configuração de menu-botao-principal no padrão.css, colocando valores em porcentagem

--- Front/src/components/menu/menu.js
+++ Front/src/components/menu/menu.js
@@ -54,11 +54,11 @@ function Menu({ link }) {
   }
 
   return (
-    <div className='principal menu-background-imagem'>
-      <div className='menu-cabeçalho linha'>
+    <div className='menu-background-imagem'>
+      <div className='menu-cabeçalho linha space-40'>
         <div className='menu-cabeçalho-esquerdo'>
         </div>
-        <div className='menu-botoes linha alinha-centro'>
+        <div className='menu-botoes linha alinha-centro espaco-entre'>
           <div className='menu-botao-painel margem-direita  alinha-centro negrito' onClick={() => irParaConsultas()}>
             <p>PAINEL</p>
           </div>
@@ -72,21 +72,21 @@ function Menu({ link }) {
         <div className='menu-cabeçalho-direito'>
         </div>
       </div>
-      <div className='menu-bem-vindo negrito'>
-        <h1 className='space-10'>Bem vindo,</h1>
+      <div className='menu-bem-vindo negrito space-20'>
+        <h1 className='space-10'>Bem vindo, &nbsp; </h1>
         <h1>{usuario}</h1>
       </div>
-      <div className='menu-mais-acessados linha negrito space-10'>
+      <div className='menu-mais-acessados linha negrito'>
         <img src={imagem} alt=""></img>
         <h1>MAIS ACESSADOS</h1>
       </div>
-      <div className='menu-botoes-principais espaco-entre'>
+      <div className='menu-botoes-principais espaco-entre alinha-centro'>
         {/* implementar um switch entre os modais, por enquanto, abre o mesmo;
             achar o erro de css que faz a página ficar desconfigurada e sem os tamanhos certos */}
-        <div className='menu-botao-principal-1 alinha-centro menu-botao-principal menu-box-sahedow negrito' onClick={() => { openModal(); setModal(1) }}>
+        <div className='menu-botao-principal-1 alinha-centro menu-botao-principal menu-box-sahedow negrito space-20' onClick={() => { openModal(); setModal(1) }}>
           <p>BUSCAR LIVRO</p>
         </div>
-        <div className='menu-botao-principal-2 alinha-centro menu-botao-principal menu-box-sahedow negrito' onClick={() => { openModal(); setModal(2) }}>
+        <div className='menu-botao-principal-2 alinha-centro menu-botao-principal menu-box-sahedow negrito space-20' onClick={() => { openModal(); setModal(2) }}>
           <p>CADASTRAR FUNCIONÁRIO</p>
         </div>
         <div className='menu-botao-principal-3 alinha-centro menu-botao-principal menu-box-sahedow negrito' onClick={() => { openModal(); setModal(3) }}>
